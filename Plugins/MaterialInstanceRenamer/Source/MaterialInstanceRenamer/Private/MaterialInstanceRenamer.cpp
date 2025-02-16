1d009035ad0547eab06e65606639d3616af7c67d
RyoehiTakada <96903403+EmbarrassingMoment@users.noreply.github.com>
2025-02-17 08:29:56 +0900

    Split each process into methods.

--- Plugins/MaterialInstanceRenamer/Source/MaterialInstanceRenamer/Private/MaterialInstanceRenamer.cpp
+++ Plugins/MaterialInstanceRenamer/Source/MaterialInstanceRenamer/Private/MaterialInstanceRenamer.cpp
@@ -14,21 +14,50 @@
 #define LOCTEXT_NAMESPACE "FMaterialInstanceRenamerModule"
 
 namespace MenuExtension_MaterialInstance {
-	static void CheckAssetReferences(const FString& AssetPath)
-	{
-		FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
-		IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
+    static void CheckAssetReferences(const FString& AssetPath)
+    {
+        FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+        IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
 
-		FAssetData AssetData = AssetRegistry.GetAssetByObjectPath(*AssetPath);
-		if (!AssetData.IsValid())
-		{
-			FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("InvalidAssetPath", "The asset path is invalid."));
-			return;
-		}
+        FAssetData AssetData = AssetRegistry.GetAssetByObjectPath(*AssetPath);
+        if (!AssetData.IsValid())
+        {
+            FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("InvalidAssetPath", "The asset path is invalid."));
+            return;
+        }
 
-		TArray<FName> Referencers;
-		AssetRegistry.GetReferencers(AssetData.PackageName, Referencers);
-	}
+        TArray<FName> Referencers;
+        AssetRegistry.GetReferencers(AssetData.PackageName, Referencers);
+    }
+
+    static FString GenerateUniqueAssetName(const FString& BaseName, const FString& PackagePath, IAssetRegistry& AssetRegistry)
+    {
+        FString FinalNewName = BaseName;
+        FString NewAssetPath = PackagePath / FinalNewName + TEXT(".") + FinalNewName;
+        int32 Suffix = 1;
+        while (AssetRegistry.GetAssetByObjectPath(*NewAssetPath).IsValid())
+        {
+            FinalNewName = BaseName + FString::Printf(TEXT("%d"), Suffix++);
+            NewAssetPath = PackagePath / FinalNewName + TEXT(".") + FinalNewName;
+        }
+        return FinalNewName;
+    }
+
+    static void HandleRenameResult(EAssetRenameResult RenameResult)
+    {
+        switch (RenameResult)
+        {
+        case EAssetRenameResult::Failure:
+            FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("RenameFailure", "Rename failed due to an unspecified error."));
+            break;
+        case EAssetRenameResult::Pending:
+            FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("RenamePending", "Rename is pending."));
+            break;
+        default:
+            FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("RenameUnknownError", "Rename failed with an unknown error."));
+            break;
+        }
+    }
 
     static void RenameMaterialInstance(const FAssetData& SelectedAsset)
     {
@@ -49,30 +78,18 @@ namespace MenuExtension_MaterialInstance {
             FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
             IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
 
-            // リネーム後の名前が存在するかチェックし、存在する場合は数値を付与
-            FString FinalNewName = NewName;
-            FString NewAssetPath = NewPackagePath / FinalNewName + TEXT(".") + FinalNewName;
-            int32 Suffix = 1;
-            while (AssetRegistry.GetAssetByObjectPath(*NewAssetPath).IsValid())
-            {
-                FinalNewName = NewName + FString::Printf(TEXT("%d"), Suffix++);
-                NewAssetPath = NewPackagePath / FinalNewName + TEXT(".") + FinalNewName;
-            }
-
+            FString FinalNewName = GenerateUniqueAssetName(NewName, NewPackagePath, AssetRegistry);
             UE_LOG(LogTemp, Log, TEXT("Renaming to: %s"), *FinalNewName);
 
-            FText OutErrorMessage;
             FAssetRenameData RenameData(SelectedAsset.GetAsset(), NewPackagePath, FinalNewName);
             EAssetRenameResult RenameResult = AssetToolsModule.Get().RenameAssetsWithDialog({ RenameData }, true);
-            bool bSuccess = (RenameResult == EAssetRenameResult::Success);
-            if (bSuccess)
+            if (RenameResult == EAssetRenameResult::Success)
             {
-                // リネーム後のアセットを再取得
+                FString NewAssetPath = NewPackagePath / FinalNewName + TEXT(".") + FinalNewName;
                 FAssetData NewAssetData = AssetRegistry.GetAssetByObjectPath(*NewAssetPath);
 
                 if (NewAssetData.IsValid())
                 {
-                    // パッケージをdirty状態にする
                     UPackage* Package = NewAssetData.GetAsset()->GetOutermost();
                     if (Package)
                     {
@@ -87,19 +104,7 @@ namespace MenuExtension_MaterialInstance {
             }
             else
             {
-                UE_LOG(LogTemp, Error, TEXT("Failed to rename asset. RenameResult: %d"), static_cast<int32>(RenameResult));
-                switch (RenameResult)
-                {
-                case EAssetRenameResult::Failure:
-                    FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("RenameFailure", "Rename failed due to an unspecified error."));
-                    break;
-                case EAssetRenameResult::Pending:
-                    FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("RenamePending", "Rename is pending."));
-                    break;
-                default:
-                    FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("RenameUnknownError", "Rename failed with an unknown error."));
-                    break;
-                }
+                HandleRenameResult(RenameResult);
             }
         }
         else
@@ -108,64 +113,64 @@ namespace MenuExtension_MaterialInstance {
         }
     }
 
-	static void OnExecuteAction(const FToolMenuContext& MenuContext)
-	{
-		UE_LOG(LogTemp, Log, TEXT("OnExecuteAction called"));
+    static void OnExecuteAction(const FToolMenuContext& MenuContext)
+    {
+        UE_LOG(LogTemp, Log, TEXT("OnExecuteAction called"));
 
- 		if (const UContentBrowserAssetContextMenuContext* Context = MenuContext.FindContext<UContentBrowserAssetContextMenuContext>())
-		{
-			UE_LOG(LogTemp, Log, TEXT("Context found"));
+        if (const UContentBrowserAssetContextMenuContext* Context = MenuContext.FindContext<UContentBrowserAssetContextMenuContext>())
+        {
+            UE_LOG(LogTemp, Log, TEXT("Context found"));
 
-			for (const FAssetData& SelectedAsset : Context->SelectedAssets)
-			{
-				UObject* Asset = SelectedAsset.GetAsset();
-				if (Asset && Asset->IsA<UMaterialInstance>())
-				{
-					RenameMaterialInstance(SelectedAsset);
-				}
-				else
-				{
-					UE_LOG(LogTemp, Log, TEXT("Selected asset is not a Material Instance"));
-					FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("NotAMaterialInstance", "The selected asset is not a Material Instance."));
-				}
-			}
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("Context not found"));
-		}
-	}
+            for (const FAssetData& SelectedAsset : Context->SelectedAssets)
+            {
+                UObject* Asset = SelectedAsset.GetAsset();
+                if (Asset && Asset->IsA<UMaterialInstance>())
+                {
+                    RenameMaterialInstance(SelectedAsset);
+                }
+                else
+                {
+                    UE_LOG(LogTemp, Log, TEXT("Selected asset is not a Material Instance"));
+                    FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("NotAMaterialInstance", "The selected asset is not a Material Instance."));
+                }
+            }
+        }
+        else
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Context not found"));
+        }
+    }
 
-	static void AddMaterialContextMenuEntry()
-	{
-		FToolMenuOwnerScoped OwnerScoped(UE_MODULE_NAME);
+    static void AddMaterialContextMenuEntry()
+    {
+        FToolMenuOwnerScoped OwnerScoped(UE_MODULE_NAME);
 
-		UToolMenu* Menu = UE::ContentBrowser::ExtendToolMenu_AssetContextMenu(UMaterialInterface::StaticClass());
+        UToolMenu* Menu = UE::ContentBrowser::ExtendToolMenu_AssetContextMenu(UMaterialInterface::StaticClass());
 
-		FToolMenuSection& Section = Menu->FindOrAddSection("GetAssetActions");
-		Section.AddDynamicEntry(NAME_None, FNewToolMenuSectionDelegate::CreateLambda([](FToolMenuSection& InSection)
-			{
-				const TAttribute<FText> Label = LOCTEXT("Material_RenameToRecommendedPrefix", "Rename to Recommended Prefix");
-				const TAttribute<FText> ToolTip = LOCTEXT("Material_RenameToRecommendedPrefixTooltip", "Rename the selected material instance to the Unreal Engine recommended prefix.");
-				const FSlateIcon Icon = FSlateIcon(FAppStyle::GetAppStyleSetName(), "ClassIcon.MaterialInstanceActor");
-				const FToolMenuExecuteAction Action = FToolMenuExecuteAction::CreateStatic(&MenuExtension_MaterialInstance::OnExecuteAction);
+        FToolMenuSection& Section = Menu->FindOrAddSection("GetAssetActions");
+        Section.AddDynamicEntry(NAME_None, FNewToolMenuSectionDelegate::CreateLambda([](FToolMenuSection& InSection)
+            {
+                const TAttribute<FText> Label = LOCTEXT("Material_RenameToRecommendedPrefix", "Rename to Recommended Prefix");
+                const TAttribute<FText> ToolTip = LOCTEXT("Material_RenameToRecommendedPrefixTooltip", "Rename the selected material instance to the Unreal Engine recommended prefix.");
+                const FSlateIcon Icon = FSlateIcon(FAppStyle::GetAppStyleSetName(), "ClassIcon.MaterialInstanceActor");
+                const FToolMenuExecuteAction Action = FToolMenuExecuteAction::CreateStatic(&MenuExtension_MaterialInstance::OnExecuteAction);
 
-				InSection.AddMenuEntry("Material_RenameToRecommendedPrefix", Label, ToolTip, Icon, Action);
-			}));
-	}
+                InSection.AddMenuEntry("Material_RenameToRecommendedPrefix", Label, ToolTip, Icon, Action);
+            }));
+    }
 }
 
 void FMaterialInstanceRenamerModule::StartupModule()
 {
-	MenuExtension_MaterialInstance::AddMaterialContextMenuEntry();
+    MenuExtension_MaterialInstance::AddMaterialContextMenuEntry();
 }
 
 void FMaterialInstanceRenamerModule::ShutdownModule()
 {
-	if (UToolMenus::IsToolMenuUIEnabled())
-	{
-		UToolMenus::UnregisterOwner(this);
-	}
+    if (UToolMenus::IsToolMenuUIEnabled())
+    {
+        UToolMenus::UnregisterOwner(this);
+    }
 }
 
 #undef LOCTEXT_NAMESPACE
